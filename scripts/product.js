cd72fcc2e77ec50ccb3d9dbf4d04320eae68932a
sakshmaverma72 <sakshmaverma72@gmail.com>
2022-12-17 22:12:09 +0530

    day-5

--- scripts/product.js
+++ scripts/product.js
@@ -2,38 +2,40 @@ let renderCards = document.querySelector(".cleanser-right-2")
 let dataURL = "https://6395eda290ac47c68077fa1c.mockapi.io/products/"
 let sortButton = document.querySelector(".sorT")
 
+
+
 async function data() {
     try {
         let res = await fetch(dataURL)
         let data = await res.json();
         // console.log(data)
         let mappedData = data.map((item) => {
-            let obj = {...item};
+            let obj = { ...item };
             obj.image = item.product_img;
             obj.name = item.product_name;
             obj.title = item.product_name2;
             obj.price = item.product_price;
             return obj;
         })
-        
-        
-        cardsRender(mappedData)     
-        sortButton.addEventListener("change",async function(e){
+
+
+        cardsRender(mappedData)
+        sortButton.addEventListener("change", async function (e) {
             let value = sortButton.value;
-            
-            if(value==="lowtohigh"){
-                let finaldata = mappedData.sort((a,b)=>
-                    a.price-b.price
+
+            if (value === "lowtohigh") {
+                let finaldata = mappedData.sort((a, b) =>
+                    a.price - b.price
                 )
                 cardsRender(finaldata)
             }
-            else if(value==="hightolow"){
-                let finaldata = mappedData.sort((a,b)=>
-                    b.price-a.price
+            else if (value === "hightolow") {
+                let finaldata = mappedData.sort((a, b) =>
+                    b.price - a.price
                 )
                 cardsRender(finaldata)
             }
-            else{
+            else {
                 cardsRender(mappedData)
             }
         });
@@ -51,14 +53,20 @@ data()
 function cardsRender(data) {
     // console.log(data)
     renderCards.innerHTML = data.map((item) => {
-        return cards(item.image, item.name, item.title, item.price,item.id)
+        return cards(item.image, item.name, item.title, item.price, item.id)
     }).join(' ')
+
+    let cart = document.querySelectorAll(".addtocart")
+    for(let button of cart){
+        button.addEventListener("click",(e)=>{
+            let cartid  = button.id;
+        });
+    }
 }
 
 
 
-
-function cards(img, name, title, price,id) {
+function cards(img, name, title, price, id) {
     return `
     <div class="cards" data-id="${id}">
         <img src=${img} alt="">
@@ -66,9 +74,9 @@ function cards(img, name, title, price,id) {
         <p>${title}</p>
         <p>RS. ${price}</p>
         <div class="hovershopper">
-            <div class="addtocart">
+            <div id = "${id}" class="addtocart ">
             <i class="fa-solid fa-bag-shopping"></i>
-            <h3>Add to Cart</h3>
+            <h3 >Add to Cart</h3>
             </div>
             <div class="favourite">
             <i class="fa-regular fa-heart"></i>
@@ -78,6 +86,7 @@ function cards(img, name, title, price,id) {
     </div>
     `
 }
+
 
 /**sort funtionality */
 
