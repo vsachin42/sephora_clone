cb0b23ff2894703f66a5402a10a03b19518ec1e9
Sachin <soniji7269@gmail.com>
2022-12-19 10:03:12 +0530

    day-6

--- scripts/addtocart.js
+++ scripts/addtocart.js
@@ -1,12 +1,17 @@
 let logo = document.querySelector("#logo");
 let login = document.querySelector("#login");
-let subTotal = document.querySelector('#subTotal').innerText;
+let subTotal = document.querySelector('#subTotal');
 let discount = document.querySelector('#discount').innerText;
 let GST = document.querySelector('#GST').innerText;
 let deliveryCharge = document.querySelector('#deliveryCharge').innerText;
 let totalPrice = document.querySelector('#totalPrice').innerText;
 let checkout = document.querySelector("#checkoutBtn");
 let shopMore = document.querySelector("#shopMoreBtn");
+let productIdURL = 'https://6395eda290ac47c68077fa1c.mockapi.io/products/';
+let productId = JSON.parse(localStorage.getItem('productId'));
+let container = document.querySelector('#productDetails');
+let productPrice = JSON.parse(localStorage.getItem('productPrice'));
+
 
 logo.addEventListener('click',()=>{
     alert('Redirecting to the landing page');
@@ -24,13 +29,61 @@ login.addEventListener('click',()=>{
 
 checkout.addEventListener('click',()=>{
     console.log('Hello');
-    alert("Hi! Let's go to checkout page");
+    alert(`Redirecting to the payment page`);
     window.location.href = 'checkout.html';
 
 })
 
 shopMore.addEventListener('click',()=>{
     alert('Go to shop more page');
+    window.location.href = 'product.html'
 })
+let total = 0;
+productId.forEach((item,index)=>{
+    async function renderData(){
+        let res = await fetch(productIdURL+item);
+        let data = await res.json();
+        // console.log(data);
+
+    let card = document.createElement('div');
+     let image = document.createElement('img');
+     image.setAttribute('src',data.product_img)
+     
+     let name = document.createElement("p");
+     name.innerText = data.product_name;
+
+     let title = document.createElement("p");
+     title.innerText = data.product_name2;
+     
+     let price = document.createElement("p");
+     price.innerText = 'Rs '+data.product_price;
+     total +=  data.product_price;
+    //  console.log(data.product_price);
+     console.log(total);
+
+     document.querySelector('#subTotal').innerText = 'Rs ' + total;
+     document.querySelector('#totalPrice').innerText = 'Rs '+ total;
+     
+     let removeButton = document.createElement('button');
+     removeButton.innerText = 'Remove';
+     removeButton.addEventListener('click',()=>{
+         alert('Product removed from cart page');
+         deleteData(productId,index);
+         location.reload();
+         total = 0;
+        })
+        
+        card.append(image,name,title,price,removeButton);
+        container.append(card);
+    }
+    renderData();
+})
+
+function deleteData(data,index){
+    data.splice(index,1);
+    localStorage.setItem('productId',JSON.stringify(data));
+}
+
+
 
 
--- scripts/product.js
+++ scripts/product.js
@@ -1,6 +1,7 @@
 let renderCards = document.querySelector(".cleanser-right-2")
 let dataURL = "https://6395eda290ac47c68077fa1c.mockapi.io/products/"
-let sortButton = document.querySelector(".sorT")
+let sortButton = document.querySelector(".sorT");
+let productId = JSON.parse(localStorage.getItem('productId')) || [];
 
 async function data() {
     try {
@@ -53,7 +54,19 @@ function cardsRender(data) {
     renderCards.innerHTML = data.map((item) => {
         return cards(item.image, item.name, item.title, item.price,item.id)
     }).join(' ');
-    console.log(data);
+    // console.log(data);
+   
+    let card = document.querySelectorAll('.addtocart');
+   for(let item of card){
+    item.addEventListener('click',()=>{
+        // console.log(document.querySelector('.cards'));
+        productId.push(+(item.id));
+        console.log(productId);
+        localStorage.setItem('productId',JSON.stringify(productId));
+        alert('Product added to the cart');
+       })
+   }
+   
 }
 
 
@@ -67,7 +80,7 @@ function cards(img, name, title, price,id) {
         <p>${title}</p>
         <p>Rs. ${price}</p>
         <div class="hovershopper">
-            <div class="addtocart">
+            <div id='${id}' class="addtocart">
             <i class="fa-solid fa-bag-shopping"></i>
             <h3>Add to Cart</h3>
             </div>
